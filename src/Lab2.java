47fd658881564f6794721943dd6e77adbc5bc51f
HST <igorparomov4@gmail.com>
2026-09-20 20:15:39 +0600

    Finished Lab2 by adding proper output and tail cleaner.

--- src/Lab2.java
+++ src/Lab2.java
@@ -3,20 +3,23 @@ import java.util.Arrays;
 public class Lab2{
     static void main() {
         Lab2 program = new Lab2();
-        int array[]={1,2,5,3,5,4};
+        int array[]={1,2,5,3,3,3,9,3,8,3,3,5,4}; //12554
         System.out.println(program.removeElementInPlace(array,3));
     }
-    public int removeElementInPlace(int[] arr, int val){
-        int valAssassinTargs = 0;
+    public boolean removeElementInPlace(int[] arr, int val){
+        int nvalAssassinTargs = 0;
         for (int i =0; i<arr.length; i++){
-            if (arr[i]==val){
-                arr[valAssassinTargs]=arr[i]; // literal mindfuckery
-                valAssassinTargs++;
+            if (arr[i]!=val){
+                arr[nvalAssassinTargs]=arr[i]; // literal mindfuckery
+                nvalAssassinTargs++;
             }
         }
-        System.out.printf("Targets killed: %d", valAssassinTargs);
-        System.out.printf("Non-targets: %s", Arrays.toString(arr));
-        return valAssassinTargs;
+        for (int i = 1; i<=(arr.length-nvalAssassinTargs);i++){
+            arr[arr.length-i]=0;
+        }
+        System.out.printf("Targets killed: %d\n", (arr.length-nvalAssassinTargs));
+        System.out.printf("Non-targets: %s\n", Arrays.toString(arr));
+        return true;
     }
 
 
